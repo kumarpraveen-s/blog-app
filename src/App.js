3dc5997db73890de671090c8d043bda4b99d834e
kumar-praveens <praveenkumars2831@gmail.com>
2022-05-31 10:21:19 +0530

    changes in code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './app.css';
+import "./App.css";
 import NavBar from "./components/navBar";
 import Article from "./pages/Article";
 import Home from "./pages/Home";
@@ -27,26 +27,26 @@ const App = () => {
       }
     };
     const applyBlog = (blogdata) => {
-      const blog=blogdata.data;
+      const blog = blogdata.data;
       setBlogs(blog);
     };
-    fetchData("https://blog-backend-react-app.herokuapp.com/api/v1/blogdata",applyBlog);
+    fetchData(
+      "https://blog-backend-react-app.herokuapp.com/api/v1/blogdata",
+      applyBlog
+    );
   }, []);
   console.log(Blogs);
   return (
     <>
-      {Blogs.length===0 && <h1 className="loader">Loading...</h1>}
-      { Blogs.length > 0  && (
+      {Blogs.length === 0 && <h1 className="loader">Loading...</h1>}
+      {Blogs.length > 0 && (
         <BrowserRouter>
           <NavBar />
           <Routes>
             <Route path="/" element={<Home Blogs={Blogs} />} />
             <Route path="/tech" element={<Tech Blogs={Blogs} />} />
             <Route path="/tours" element={<Tours Blogs={Blogs} />} />
-            <Route
-              path="/fitness"
-              element={<Fitness Blogs={Blogs} />}
-            />
+            <Route path="/fitness" element={<Fitness Blogs={Blogs} />} />
             <Route path="/article/:id" element={<Article />} />
           </Routes>
         </BrowserRouter>
